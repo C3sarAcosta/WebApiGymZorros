97d5c8b2df09dd9411e6fc9e28d766d6a40401a8
César Acosta <c3sar.adan@gmail.com>
2022-07-21 18:57:26 -0600

    Agregar archivos de proyecto.

---
+++ WebApiGymZorros/Contextos/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebApiGymZorros.Entidades;
+
+namespace WebApiGymZorros.Contextos
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+        
+        public DbSet<ApplicationUser> Usuarios { get; set; }
+    }
+}
---
+++ WebApiGymZorros/Entidades/ApplicationUser.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace WebApiGymZorros.Entidades
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string Nombres { get; set; }
+        public string ApellidoPaterno { get; set; }
+        public string ApellidoMaterno { get; set; }
+        public string Sexo { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string Colonia { get; set; }
+        public string Calle { get; set; }
+        public int NumeroExterior { get; set; }
+        public int NumeroInterior { get; set; }
+        public string Telefono { get; set; }
+    }
+}
